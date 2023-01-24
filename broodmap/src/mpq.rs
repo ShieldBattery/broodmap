2871f864db8bb4b3c180a21251562309a03e10d2
Travis Collins <travis@tec27.com>
2023-01-24 01:02:05 -0800

    Initial commit.

---
+++ broodmap/src/mpq.rs
@@ -0,0 +1,4 @@
+//! An implementation of the MPQ file format, for the features used by SCM/SCX files in StarCraft.
+//! All features that are not used by the game are not implemented, and the implementation matches
+//! the game's behavior as closely as possible (including bugs, which are often exploited by
+//! so-called "map protectors" to prevent 3rd party editors from opening map files).
